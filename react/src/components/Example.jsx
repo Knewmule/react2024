e55c739d9f84501cd8bfa788758b7838162ea7b9
bigproblems55 <pro@core>
2024-01-07 14:27:44 -0800

    Added string identifier wraper

--- react/src/components/Example.jsx
+++ react/src/components/Example.jsx
@@ -3,6 +3,7 @@ import { EXAMPLES} from '../data.js';
 import Examples from "./Examples/Examples";
 import TabButton from "./TabButton";
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 export default function Example(){
   // Only call hooks inside component Funcition
   // Only call hooks on the top level
@@ -13,14 +14,23 @@ export default function Example(){
     
   }
     return(
-        <Section title="Examples"id="examples">
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'}onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'}onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'}onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'}onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          <Examples selectedTopic={selectedTopic} {...EXAMPLES[selectedTopic] } />
+        <Section title="Examples" id="examples">
+          <Tabs buttonsContainer="menu" buttons={
+            <>
+            <TabButton isSelected={selectedTopic === 'components'}
+        onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}
+            onClick={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}
+            onClick={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}
+            onClick={() => handleSelect('state')}>State</TabButton>
+            </>
+          }><Examples selectedTopic={selectedTopic} 
+          {...EXAMPLES[selectedTopic] } />
+          </Tabs>
+          
+          
         </Section>
     );
 }
