ada15fab316d643e686483477b48f84e7e5815d7
bigproblems55 <pro@core>
2024-01-16 12:21:02 -0800

    Using forward ref with modal

--- 01-starting-project/src/components/TimerChallenge.jsx
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -4,9 +4,12 @@ export default function TimerChallenge({title, targetTime}){
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
     const timer = useRef();    
+    const dialog = useRef();
     function handleStart(){
+
         timer.current = setTimeout(() =>{
             setTimerExpired(true);
+            dialog.current.showModal();
         },targetTime *1000);
         setTimerStarted(true);
     }
@@ -15,7 +18,7 @@ export default function TimerChallenge({title, targetTime}){
     }
     return(
         <>
-        {timerExpired && <ResultModal  targetTime={targetTime} result="lost"/>}
+         <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
         <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
