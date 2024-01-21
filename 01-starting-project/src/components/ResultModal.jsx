60dda529eeb80c06f4d44e11469982a01b8de515
bigproblems55 <pro@core>
2024-01-21 10:53:12 -0800

    You lost message and timer inactive or active messages along with the amount of time left

--- 01-starting-project/src/components/ResultModal.jsx
+++ 01-starting-project/src/components/ResultModal.jsx
@@ -1,7 +1,9 @@
 import {forwardRef, useImperativeHandle,useRef} from 'react';
 import { createPortal } from 'react-dom';
-const ResultModal = forwardRef(function ResultModal({result,targetTime},ref) {
+const ResultModal = forwardRef(function ResultModal({onReset,targetTime,remainingTime},ref) {
     const dialog = useRef();
+    const userLost = remainingTime <= 0;
+    const formattedRemaningTime = (remainingTime/1000).toFixed(2);
     useImperativeHandle(ref, ()=>{
         return{
             open() {
@@ -11,10 +13,10 @@ const ResultModal = forwardRef(function ResultModal({result,targetTime},ref) {
     });
     return (
         <dialog ref={dialog} className="result-modal" >
-            <h2>You {result}</h2>
+            {userLost && <h2>You Lost</h2>}
             <p>The target time was <strong>{targetTime}</strong></p>
-            <p>You stopped the timer with <strong> X seconds left</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong> {formattedRemaningTime} seconds left</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
