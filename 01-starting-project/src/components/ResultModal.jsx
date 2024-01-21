0eb1fb5601c4fc29954d20c03645e653b57e6ecd
bigproblems55 <pro@core>
2024-01-21 11:01:40 -0800

    Added escape key function with onclock reset

--- 01-starting-project/src/components/ResultModal.jsx
+++ 01-starting-project/src/components/ResultModal.jsx
@@ -4,6 +4,7 @@ const ResultModal = forwardRef(function ResultModal({onReset,targetTime,remainin
     const dialog = useRef();
     const userLost = remainingTime <= 0;
     const formattedRemaningTime = (remainingTime/1000).toFixed(2);
+    const score = Math.round((1 - remainingTime /(targetTime * 1000) ) * 100);
     useImperativeHandle(ref, ()=>{
         return{
             open() {
@@ -12,8 +13,9 @@ const ResultModal = forwardRef(function ResultModal({onReset,targetTime,remainin
         };
     });
     return (
-        <dialog ref={dialog} className="result-modal" >
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime}</strong></p>
             <p>You stopped the timer with <strong> {formattedRemaningTime} seconds left</strong></p>
             <form method="dialog" onSubmit={onReset}>
