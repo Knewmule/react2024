5a66de80b416c90713ffe60a4ac5249d14dfede1
bigproblems55 <pro@core>
2024-01-22 00:05:04 -0800

    Added new project buttons

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,34 @@
+
 import ProjectsSidebar from "./components/ProjectsSidebar";
-import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 function App() {
+  const [projectState,setProjectState] = useState(
+    {selectedProjectId: undefined,
+    projects:[]
+    }
+  );
+
+  function handleStartAddProject(){
+    setProjectState(prevState =>{
+      return{
+        ...prevState,
+        selectedProjectId:null
+      }
+    })
+  }
+  let content;
+  if(projectState.selectedProjectId === null){
+    content = <NewProject />;
+
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
