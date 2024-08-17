b4cb90efd766e4427628aa782d9b9fef355bf42a
vivit <jack.problems@protonmail.com>
2024-08-16 20:59:05 -0700

    added and remove favorite places closest to your geographic location

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -14,8 +14,8 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
   );
 function App() {
   
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen,setModalIsOpen] = useState(false);
   const [availablePlaces,setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -34,12 +34,12 @@ function App() {
   },[])
   
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -70,7 +70,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal ref={modal} open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
