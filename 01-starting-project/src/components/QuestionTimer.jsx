4c63532bdfb31bebaf83c0cb5967dba41cd5c80c
vivit <jack.problems@protonmail.com>
2024-08-18 12:30:41 -0700

    Works for the teacher but for me all the buttons light up instead of just the one you pressed

--- 01-starting-project/src/components/QuestionTimer.jsx
+++ 01-starting-project/src/components/QuestionTimer.jsx
@@ -4,14 +4,23 @@ export default function QuestionTimer({timeout,onTimeout}) {
     const [remaingTime,setRemainingTime] = useState(timeout);
     
     useEffect(()=>{
-        setTimeout(onTimeout,timeout);
+        console.log("Setting Timeout");
+        const timer = setTimeout(onTimeout,timeout);
+        return () =>{
+            clearTimeout(timer);
+        }
 
     },[timeout,onTimeout])
     useEffect(() =>{
-        setInterval(()=>{
+        console.log("setting Interval");
+        const interval =   setInterval(()=>{
             setRemainingTime(prevRemaingTime => prevRemaingTime - 100);
         }, 100)
-    }, [])
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
     
     return <progress id="question-time" max={timeout} value={remaingTime}/>;
 }
--- 01-starting-project/src/components/Quiz.jsx
+++ 01-starting-project/src/components/Quiz.jsx
@@ -1,45 +1,51 @@
-import { act, useState } from 'react';
+import { react, useState,useCallback,useRef } from 'react';
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 export default function Quiz() {
     
+    const [answerState,setAnswerState] = useState('');
     const [userAnswers, setUserAnswer] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1;
     
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnswerState('answered');
         setUserAnswer((prevUserAnswer) => {
             return[...prevUserAnswer, selectedAnswer];
         });
-    }
 
+        setTimeout(() =>{
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+                setAnswerState('correct');
+            }else{
+                setAnswerState('wrong');
+            }
+            setTimeout(() =>{
+                setAnswerState('');
+            },2000)
+        },1000)
+    },[activeQuestionIndex]);
+    const handleSkipAnswer = useCallback(() =>handleSelectAnswer(null), [handleSelectAnswer]);
     if (quizIsComplete){
         return <div id="summary">
             <img src={quizCompleteImg} alt="Trphy Quiz"/>
             <h2>Quiz Completed!</h2>
         </div>
     }
+    
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(()=> Math.random() -0.5);
-
+// We can use the keyi prop to force react to reload and destroy the component
     return <div id="quiz" >
-        <div id="question">
-        <QuestionTimer timeout={10000} 
-        onTimeout={() => handleSelectAnswer(null)
-        }/>
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-            {shuffledAnswers.map(answer => 
-                <li key={answer} className='answer'>
-                    <button  onClick={() => handleSelectAnswer(answer)}>
-                        {answer}
-                    </button>
-                </li>
-            )}
-        </ul>
-    </div>
+        <Question 
+        key={activeQuestionIndex}
+        questionText={QUESTIONS[activeQuestionIndex].text}
+            answers={QUESTIONS[activeQuestionIndex].answers}
+            onSelectAnswer={handleSelectAnswer}
+            onSkipAnswer={handleSkipAnswer}
+            selectedAnswer={userAnswers[userAnswers.length -1] }
+            answerState={answerState}
+        />
     </div>
 }
