debed0e96a6154a3466bc00e0b925517bd528598
vivit <jack.problems@protonmail.com>
2024-08-27 17:23:04 -0700

    Not getting the same as the teacher

--- 01-starting-project/src/components/Quiz.jsx
+++ 01-starting-project/src/components/Quiz.jsx
@@ -1,12 +1,13 @@
-import { react, useState,useCallback,useRef } from 'react';
+import { react, useState,useCallback,useRef, act } from 'react';
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from './Question.jsx';
 export default function Quiz() {
-    
+    const shuffledAnswers = useRef();
     const [answerState,setAnswerState] = useState('');
     const [userAnswers, setUserAnswer] = useState([]);
-    const activeQuestionIndex =  userAnswers.length;
+    
+    const activeQuestionIndex = answerState === '' ? userAnswers.length: userAnswers.length -1;
     
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
@@ -34,16 +35,31 @@ export default function Quiz() {
             <h2>Quiz Completed!</h2>
         </div>
     }
-    
+    if(!shuffledAnswers.current){
+        shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.current.sort(() => Math.random() -0.5)
+    }
 
 // We can use the keyi prop to force react to reload and destroy the component
     return(
-        <div id="quiz" >
-        <Question 
-            k={activeQuestionIndex}
-            onSelectAnswer={handleSelectAnswer}
-            onSkipAnswer={handleSkipAnswer}
-        />
+    <div id="quiz" >
+        <div id="question">
+        <QuestionTimer 
+            key={activeQuestionIndex}
+            timeout={10000}
+            onTimeout={handleSkipAnswer}
+        />    
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer) =>{
+                const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                let cssClass = '';
+                if (answerState === 'answered' && isSelected){
+                    cssClass = 'selected';
+                }
+            })}
+        </ul>
+        </div>    
     </div>
     );
 }
