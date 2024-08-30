a7b3fd6d47cdf9b3be0c398bc4da96e63d2ffe8b
vivit <jack.problems@protonmail.com>
2024-08-30 14:05:50 -0700

    cannot read property of undefined react create element for app 22 with the fragment component seems to have an issue iwht it. I also had issues with vite. It would not admit vite.config.js was in the project so I didnt include it and it now works however it does not seem to think the teachers code was edible in the firstplace so Now nothing starts off the way the teacher inttended

---
+++ 01-starting-project/dev/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+window.React = React;
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
