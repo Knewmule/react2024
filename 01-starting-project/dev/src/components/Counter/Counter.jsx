f7dc19f05a48172b764353c4c33f1cd7c0778e72
vivit <jack.problems@protonmail.com>
2024-08-30 19:18:28 -0700

    We have Components rendering once using memo and usememo and callback and by targetting higher order components on the component tree with these functions make seperate components that dont rerender for no reason

--- 01-starting-project/dev/src/components/Counter/Counter.jsx
+++ 01-starting-project/dev/src/components/Counter/Counter.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import { useState, memo, useCallback,useMemo} from 'react';
+import CounterHistory from './CounterHistory.jsx'
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
@@ -26,36 +26,44 @@ function isPrime(number) {
 
   return true;
 }
-
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
-
-  const [counter, setCounter] = useState(initialCount);
-
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+// Memo keeps the compoent from rendering again by checking the props and comparing it to the next one
+// when it gets rendered again in app memo will see prop never changed and not allow counter to rerender
+// Memo only cares about external changes not internal ones. It should not be used for all components
+// Only use memo for components that are high up on the component tree
+const Counter = memo (
+  function Counter({ initialCount }) {
+    log('<Counter /> rendered', 1);
+    const initialCountIsPrime = useMemo(() =>isPrime(initialCount), [initialCount]);
+  
+    const [counter, setCounter] = useState(initialCount);
+  
+    const handleDecrement = useCallback(function handleDecrement() {
+      setCounter((prevCounter) => prevCounter - 1);
+    },[])
+  
+    const handleIncrement = useCallback(
+      function handleIncrement() {
+        setCounter((prevCounter) => prevCounter + 1);
+      },[]);
+  
+    return (
+      <section className="counter">
+        <p className="counter-info">
+          The initial counter value was <strong>{initialCount}</strong>. It{' '}
+          <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        </p>
+        <p>
+          <IconButton icon={MinusIcon} onClick={handleDecrement}>
+            Decrement
+          </IconButton>
+          <CounterOutput value={counter} />
+          <IconButton icon={PlusIcon} onClick={handleIncrement}>
+            Increment
+          </IconButton>
+        </p>
+          
+      </section>
+    );
   }
-
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
-
-  return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-      </p>
-      <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
-          Decrement
-        </IconButton>
-        <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
-          Increment
-        </IconButton>
-      </p>
-    </section>
-  );
-}
+)
+export default Counter;
--- 01-starting-project/dev/src/main.jsx
+++ 01-starting-project/dev/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 window.React = React;
 import App from './App.jsx';
 import './index.css';
