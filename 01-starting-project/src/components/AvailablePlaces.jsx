dd6020b12f279cc22c58b457826f9b42b76c30de
vivit <jack.problems@protonmail.com>
2024-09-01 22:40:01 -0700

    STart off proj fullstck

---
+++ 01-starting-project/src/components/AvailablePlaces.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  return (
+    <Places
+      title="Available Places"
+      places={[]}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
