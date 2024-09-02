8b22151606d649ab05e6dfb28eee4305f7849fa0
vivit <jack.problems@protonmail.com>
2024-09-01 23:59:59 -0700

    fetches the request asynchronsly

--- 01-starting-project/src/components/AvailablePlaces.jsx
+++ 01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+
+import React,{useState,useEffect} from 'react';
 import Places from './Places.jsx';
 
+const places = localStorage.getItem('places');
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(()=>{
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places')
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
+    //   fetch('http://localhost:3000/places')
+    // .then((response) => {
+    //   return response.json()
+    // })
+    // .then((resData) => {
+    //   setAvailablePlaces(resData.places);
+    // });
+  },[])
+
+  // const response = await fetch('http://localhost:3000/places')
   return (
     <Places
-      title="Available Places"
-      places={[]}
+      title={availablePlaces.title}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
