2da741232570d4db0db5d689ae581980ecedfd30
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-02 16:56:25 -0700

    Section 16 Custom React Hooks start off

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,33 +1,40 @@
-import React, { useRef, useState, useCallback, useEffect } from 'react';
-import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import React,{ useRef, useState, useCallback, useEffect } from 'react';
+
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import ErrorPage from './components/Error.jsx';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
+
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching,setIsFetching] = useState(false); 
-  const [error,setError] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() =>{
-    async function fetchPlaces(){
+  useEffect(() => {
+    async function fetchPlaces() {
       setIsFetching(true);
-      try{
-        const places  = await fetchUserPlaces();  
-      setUserPlaces(places);
-      }catch(error){
-        setError({message: error.message || 'Failed to fetch user palces'})
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch user places.' });
       }
-      setIsFetching(true);
+
+      setIsFetching(false);
     }
+
     fetchPlaces();
   }, []);
+
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
     selectedPlace.current = place;
@@ -38,6 +45,8 @@ function App() {
   }
 
   async function handleSelectPlace(selectedPlace) {
+    // await updateUserPlaces([selectedPlace, ...userPlaces]);
+
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -47,43 +56,57 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
-    try{
-      await updateUserPlaces([selectedPlace,...userPlaces])
-    }catch(error){
+
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
       setUserPlaces(userPlaces);
-      setErrorUpdatingPlaces({message:error.message || 'Failed to update places'});
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to update places.',
+      });
     }
   }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
-    try{
-      await updateUserPlaces(
-        userPlaces.filter(place => place.id !== selectedPlace.current.id)
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id
+        )
       );
-    }catch(error){
-      setUserPlaces(userPlaces);
-      setErrorUpdatingPlaces({message: error.message ||  'Failed to delete place.'})
-    }
-    setModalIsOpen(false);
-  }, [userPlaces]);
 
+      try {
+        await updateUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+        );
+      } catch (error) {
+        setUserPlaces(userPlaces);
+        setErrorUpdatingPlaces({
+          message: error.message || 'Failed to delete place.',
+        });
+      }
 
-  function handleError(){
+      setModalIsOpen(false);
+    },
+    [userPlaces]
+  );
+
+  function handleError() {
     setErrorUpdatingPlaces(null);
   }
+
   return (
     <>
       <Modal open={errorUpdatingPlaces} onClose={handleError}>
         {errorUpdatingPlaces && (
-          <ErrorPage title="An error has occured" 
-          message={errorUpdatingPlaces.message}
-          onConfirm={handleError}
+          <Error
+            title="An error occurred!"
+            message={errorUpdatingPlaces.message}
+            onConfirm={handleError}
           />
         )}
       </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
@@ -100,18 +123,17 @@ function App() {
         </p>
       </header>
       <main>
-        {error && <ErrorPage title="An error occured!" message={error.message}/>}
+        {error && <Error title="An error occurred!" message={error.message} />}
         {!error && (
           <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          isLoading={isFetching}
-          loadingText="Fetching your places.."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        )
-        }
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            isLoading={isFetching}
+            loadingText="Fetching your places..."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- 01-starting-project/src/components/Places.jsx
+++ 01-starting-project/src/components/Places.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
-export default function Places({ title, places, fallbackText, onSelectPlace,isLoading,loadingText }) {
+import React from 'react'
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isLoading,
+  loadingText,
+}) {
   console.log(places);
   return (
     <section className="places-category">
@@ -11,7 +18,10 @@ export default function Places({ title, places, fallbackText, onSelectPlace,isLo
           {places.map((place) => (
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                <img
+                  src={`http://localhost:3000/${place.image.src}`}
+                  alt={place.image.alt}
+                />
                 <h3>{place.title}</h3>
               </button>
             </li>
