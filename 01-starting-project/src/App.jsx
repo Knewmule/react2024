c21fed2dc9e7ea2fa08af78b85ffc67d7f58b25c
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-02 19:35:42 -0700

    Converting useEffect and State into a hook on the outside component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React,{ useRef, useState, useCallback, useEffect } from 'react';
-
+import {useFetch} from './hooks/useFetch.js'
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
@@ -11,29 +11,15 @@ import Error from './components/Error.jsx';
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({ message: error.message || 'Failed to fetch user places.' });
-      }
-
-      setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
+  const {isFetching, 
+    error, 
+    fetchedData:userPlaces,
+    setFetchedData: setUserPlaces
+    } = useFetch(fetchUserPlaces,[]);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -88,7 +74,7 @@ function App() {
 
       setModalIsOpen(false);
     },
-    [userPlaces]
+    [userPlaces,setUserPlaces]
   );
 
   function handleError() {
@@ -107,7 +93,9 @@ function App() {
         )}
       </Modal>
 
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+      <Modal open={modalIsOpen} 
+      onClose={handleStopRemovePlace}
+      >
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -135,7 +123,9 @@ function App() {
           />
         )}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces 
+        onSelectPlace={handleSelectPlace}
+         />
       </main>
     </>
   );
