d12c3579311ee93a1195d23460c2e2269b85ab50
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-13 12:08:56 -0700

    form with password and email

---
+++ 01-starting-project/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React,{useState} from "react";
+
+
+export default function Login() {
+
+  const [enteredValue,setEnteredValue] =useState({
+    email:'',password:'',
+  })
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log('Submitted'+enteredValue.email)
+
+  }
+  function handleInputChange(identifier,value){
+    setEnteredValue(preveValues => ({
+      ...preveValues,[identifier]:value
+    }))
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" 
+          onChange={(event) =>handleInputChange('email',event.target.value)} 
+          value={enteredValue.email} />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password"  
+          onChange={(event) =>handleInputChange('password',event.target.value)} 
+          value={enteredValue.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button type="button"className="button" onClick={handleSubmit}>Login</button>
+      </p>
+    </form>
+  );
+}
