36273c0e945e02a0317e04e6dd9c992d181edea0
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-13 21:00:40 -0700

    make two email boxes instead of a password input

--- 01-starting-project/src/components/StateLogin.jsx
+++ 01-starting-project/src/components/StateLogin.jsx
@@ -1,57 +1,74 @@
 import React,{useState} from "react";
 import Input from './Input';
-import {isEmail, isNotEmpty, hasMinLength } from '../util/validation'
+import {isEmail, isNotEmpty, hasMinLength } from '../util/validation';
+import {useInput} from '../hooks/useInput'; 
 export default function StateLogin() {
+const {value: emailValue,
+     handleInputChange: handleEmailChange,
+     handleInputBlur: handleEmailBlur,
+     hasError: emailHasError,
+}= useInput('',(value)=>{ isEmail(value) && isNotEmpty(value)});
 
-  const [enteredValue,setEnteredValue] =useState({
-    email:'',password:'',
-  })
+    const {value: passwordValue, 
+        handleInputChange: handlePasswordChange,
+        handleInputBlur: handlePasswordBlur,
+        hasError: passwordHasError
+    } = useState('',(value) => hasMinLength(value,6));
+//   const [enteredValue,setEnteredValue] =useState({
+//     email:'',password:'',
+//   })
 
-  const [didEdit,setDidEdit] = useState({
-    email:false,password:false,
-  })
-  const emailIsInvalid = didEdit.email && !enteredValue.email.includes('@');
-  const passwordIsInvalid = didEdit.password && enteredValue.password.trim().length <6;
+//   const [didEdit,setDidEdit] = useState({
+//     email:false,password:false,
+//   })
+
+
+//   const emailIsInvalid = didEdit.email && 
+//   !isEmail(enteredValue.email) && 
+//   !isNotEmpty(enteredValue.email)
+//   const passwordIsInvalid = didEdit.password && !hasMinLength(enteredValue.password,6);
+
   function handleSubmit(event){
     event.preventDefault();
-    console.log('Submitted'+enteredValue.email);
-    setEnteredValue({
-        email:'',password:'',
-      });
+    
+    if(emailHasError || passwordHasError) {
+        return;
+    }
+    console.log('Submitted'+emailValue,passwordValue);
 
   }
-  function handleInputChange(identifier,value){
-    setEnteredValue(preveValues => ({
-      ...preveValues,[identifier]:value
-    }))
-    setDidEdit((prevEdit) =>({
-        ...prevEdit,
-        [identifier]:false
-    }))
-  }
+//   function handleInputChange(identifier,value){
+//     setEnteredValue(preveValues => ({
+//       ...preveValues,[identifier]:value
+//     }))
+//     setDidEdit((prevEdit) =>({
+//         ...prevEdit,
+//         [identifier]:false
+//     }))
+//   }
 
-  function handleInputBlur(identifier){
-    setDidEdit(prevEdit => ({
-        ...prevEdit,
-        [identifier]: true
-    }))
-  }
+//   function handleInputBlur(identifier){
+//     setDidEdit(prevEdit => ({
+//         ...prevEdit,
+//         [identifier]: true
+//     }))
+  
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <Input label="email" id="email" name="email" 
-        onChange={(event) =>handleInputChange('email',event.target.value)} 
-        value={enteredValue.email} 
-        error={emailIsInvalid && 'Please enter a vlid email!'}
-        onBlur={() => handleInputBlur('email')} />
+        onChange={handleEmailChange} 
+        value={emailValue} 
+        error={emailHasError && 'Please enter a vlid email!'}
+        onBlur={handleEmailBlur} />
 
         <Input label="password" id="password" name="password" 
-        onChange={(event) =>handleInputChange('password',event.target.value)} 
-        value={enteredValue.password} 
-        error={passwordIsInvalid && 'Please enter a valid password!'}
-        onBlur={() => handleInputBlur('password')} />
+        onChange={handlePasswordChange} 
+        value={passwordValue} 
+        error={passwordHasError && 'Please enter a valid password!'}
+        onBlur={handlePasswordBlur} />
 
       </div>
 
